a7c555ce969080bc9639dde89635a5e8115e57ab
Steve Crow <www.email@pm.cheesecova.com>
2024-11-28 18:30:04 -0500

    Apply license

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
-	<?php
+<?php
+/*
+
+ykweb - Web based TOTP code access for Yubikeys
+github.com/scrow/ykweb
+Copyright (C) 2024 Steven Crow
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 $system_ver = "1.0";
 $system_rel = "2024-11-28";
 $page_footer = 'ykweb ' . $system_ver . ' by Steve Crow, <A HREF="https://github.com/scrow/ykweb" TARGET="_blank">github.com/scrow/ykweb</A>';
